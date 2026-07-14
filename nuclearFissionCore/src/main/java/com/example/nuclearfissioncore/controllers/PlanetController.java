297892ce79905b554af1bb7063f396944deb1eb4
knelsonramp <knelson@ramphealth.com>
2026-07-13 22:44:30 -0400

    Added get planets API request

--- nuclearFissionCore/src/main/java/com/example/nuclearfissioncore/controllers/PlanetController.java
+++ nuclearFissionCore/src/main/java/com/example/nuclearfissioncore/controllers/PlanetController.java
@@ -5,6 +5,7 @@ import com.example.nuclearfissioncore.repositoryies.PlanetRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -17,12 +18,11 @@ public class HelloController {
 
     @GetMapping("/planets/{id}")
     public Planet getPlanet(@PathVariable Integer id) {
-        return new Planet("A");
-//        return planetRepository.findById(id).orElse(null);
+        return planetRepository.findById(id).orElse(null);
     }
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "nuclear fuzzbuzz";
+    @GetMapping("/planets")
+    public List<Planet> getAllPlanets() {
+        return planetRepository.findAll();
     }
 }
