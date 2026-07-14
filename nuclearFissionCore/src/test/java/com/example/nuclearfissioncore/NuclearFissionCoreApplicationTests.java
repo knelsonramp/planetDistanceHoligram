c579047c0eec9df58bd8e51d7634297caa21c6a1
knelsonramp <knelson@ramphealth.com>
2026-07-13 20:40:08 -0400

    added nuclearFisionCore aka backend

---
+++ nuclearFissionCore/src/test/java/com/example/nuclearfissioncore/NuclearFissionCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nuclearfissioncore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuclearFissionCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
