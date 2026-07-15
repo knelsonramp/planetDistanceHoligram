417d9661c8bc37f4854e66c8990ba9a7aec926da
knelsonramp <knelson@ramphealth.com>
2026-07-15 18:51:30 -0400

    call planets and list them

--- holigram/src/app/app.ts
+++ holigram/src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('holigram');
+
+  planets: any[] = [];
+
+    constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any[]>('http://localhost:8080/planetsWithRoutes')
+      .subscribe({
+        next: (data) => {
+          this.planets = data;
+          console.log(data);
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      });
+  }
 }
