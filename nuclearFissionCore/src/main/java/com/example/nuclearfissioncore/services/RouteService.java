a890371835b53ed4739f225c5c74f19357b7b07d
knelsonramp <knelson@ramphealth.com>
2026-07-15 11:52:14 -0400

    Added min path to destination logic

--- nuclearFissionCore/src/main/java/com/example/nuclearfissioncore/services/RouteService.java
+++ nuclearFissionCore/src/main/java/com/example/nuclearfissioncore/services/RouteService.java
@@ -4,6 +4,7 @@ import com.example.nuclearfissioncore.models.Route;
 import com.example.nuclearfissioncore.repositoryies.RouteRepository;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.Path;
 import java.util.*;
 
 @Service
@@ -20,6 +21,64 @@ public class RouteService {
             return 0.0;
         }
 
-        return -1.0;
+        Queue<PathTracker> pathsToExplore = new ArrayDeque<>();
+
+        List<Integer> startingPoint = new ArrayList<>(List.of(originPlanetId));
+        double currentDistance = 0;
+        PathTracker startingPath = new PathTracker(startingPoint, currentDistance);
+        pathsToExplore.add(startingPath);
+
+        double minDistance = Double.POSITIVE_INFINITY;
+        while(!pathsToExplore.isEmpty()) {
+            PathTracker currentPath = pathsToExplore.poll();
+
+            if(currentPath.distance > minDistance) {
+                continue;
+            }
+
+            int lastestPlanetIdInPath = currentPath.path.get(currentPath.path.size() - 1);
+
+            if(Objects.equals(lastestPlanetIdInPath, destinationPlanetId)) {
+                minDistance = currentPath.distance;
+                continue;
+            }
+
+            List<Route> departureRoutes = routeRepository.findByOriginPlanetId(lastestPlanetIdInPath);
+
+            for(Route departureRoute: departureRoutes) {
+                if(currentPath.path.contains(departureRoute.getDestinationPlanetId())) {
+                    continue;
+                }
+
+                List<Integer> newPath = new ArrayList<>(currentPath.path);
+                newPath.add(departureRoute.getDestinationPlanetId());
+                double newDistance = currentPath.distance + departureRoute.getDistance();
+                PathTracker pathToExplore = new PathTracker(newPath, newDistance);
+                pathsToExplore.add(pathToExplore);
+            }
+        }
+
+        return minDistance;
+    }
+
+    static class PathTracker {
+        List<Integer> path;
+        double distance;
+
+        PathTracker(List<Integer> currentPath, double currentDistance) {
+            this.path = currentPath;
+            this.distance = currentDistance;
+        }
+    }
+
+    private List<Integer> getDestinationPlanetIdsFromRoutes(List<Route> departureRoutes) {
+        if(departureRoutes.isEmpty()) {
+            return new ArrayList<>();
+        }
+        List<Integer> destinationPlanetIds = new ArrayList<>();
+        for(Route route: departureRoutes) {
+            destinationPlanetIds.add(route.getDestinationPlanetId());
+        }
+        return destinationPlanetIds;
     }
 }
